900fd6ad8ed870b8b59da053e5b2bb6ec442aa4e
ent99 <55948906+ent99@users.noreply.github.com>
2019-11-11 11:46:11 +0000

    Add files via upload

---
+++ dbConfig.php
@@ -0,0 +1,17 @@
+<?php
+//DB details
+$dbHost = 'localhost';
+$dbUsername = 'root';
+dbPassword = '';
+$dbName = 'shoppingcart';
+
+//Create connection and select DB
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+if ($db->connect_error) {
+    die("Unable to connect database: " . $db->connect_error);
+}
+
+echo"great work";
+
+?>
